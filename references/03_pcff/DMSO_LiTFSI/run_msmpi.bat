fc3a3afc77c75c6d4ce6deaef66ca3bfa60ec0d6
by-student-2017 <studentsctest@gmail.com>
2023-07-18 17:59:31 +0900

    windows11

---
+++ references/03_pcff/DMSO_LiTFSI/run_msmpi.bat
@@ -0,0 +1,15 @@
+set ncore=4
+
+mkdir cfg
+
+for %%1 in (DMSO_LiTFSI_min_short_nvt.lmp) do (
+  "C:\Program Files\Microsoft MPI\Bin\mpiexec.exe" -np %ncore% "C:\Program Files\LAMMPS 64-bit 22Dec2022-MSMPI\bin\lmp.exe" -in %%1
+  echo input file: %%1
+)
+
+for %%1 in (DMSO_LiTFSI_long_nvt.lmp) do (
+  "C:\Program Files\Microsoft MPI\Bin\mpiexec.exe" -np %ncore% "C:\Program Files\LAMMPS 64-bit 22Dec2022-MSMPI\bin\lmp.exe" -in %%1
+  echo input file: %%1
+)
+
+pause
